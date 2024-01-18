3508f826bab0941736512e997035a1ed9eef14b9
ella-jones <14447@coderacademy.edu.au>
2024-01-18 20:46:47 +0800

    feat: start on home page hero section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,9 @@
 import Logo from "../assets/ella_logo.png";
+import Headshot from "../assets/ella_headshot.png"
 
 const images = {
     Logo,
+    Headshot,
 };
 
 export default images;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,34 @@
 import React from "react";
 
 import MainLayout from "../components/MainLayout";
+import { images } from "../constants";
+import { NavLink } from "react-router-dom";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const HomePage = () => {
     return (
         <MainLayout>
-            
+            <section className="flex justify-between px-28 pt-20 ">
+                <div className="w-1/4">
+                    <div className="text-5xl font-semibold">
+                        <div className="flex gap-2">
+                            <h1>Hi, I'm </h1>
+                            <h1 className="text-tangerine">ella,</h1>
+                        </div>
+                        <h1>A junior full-stack</h1>
+                        <h1>web-developer.</h1>
+                    </div>
+                    <p className="mt-5">I create bold websites for daring brands. I create bold websites for daring brands. I create bold websites for daring brands.</p>
+                    <NavLink to="/Projects">
+                        <button className="flex items-center gap-2 border-2 text-tangerine border-tangerine rounded-3xl px-4 py-2 mt-5">
+                            <h1>My Work</h1>
+                            <FaArrowRightLong />
+                        </button>
+                    </NavLink>
+
+                </div>
+                <img src={images.Headshot} alt="Ella Jones Headshot" className="h-96 rounded-t-full rounded-b-3xl "/>
+            </section>
         </MainLayout>
     );
 };
